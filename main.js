e0af96b8dbffd5a7673c37045edc15c63ef4e53e
Long Hai Nguyen <longhai14149@gmail.com>
2025-07-03 09:57:19 +0700

    added related content part, still some bugs of displaying multiple modals

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const $$ = document.querySelectorAll.bind(document);
         </div>
 </article> */
 
+Modal.elements = [];
+
 function Modal(options = {}) {
     const { templateId,
             closeMethods = ['button', 'overlay', 'escape'],
@@ -110,6 +112,8 @@ function Modal(options = {}) {
     }
 
     this.open = () => {
+        Modal.elements.push(this);
+
         if (!this._backdrop) {
             this._build();
         }
@@ -129,11 +133,7 @@ function Modal(options = {}) {
         }
         
         if (this._allowEscapeClose) {
-            document.addEventListener('keydown', (e)=> {
-                if (e.key === 'Escape') {
-                    this.close();
-                }
-            })
+            document.addEventListener('keydown', this._handleEscapeKey);
         }
 
         this._onTransitionEnd(() => {
@@ -145,6 +145,12 @@ function Modal(options = {}) {
         document.body.style.paddingRight = getScrollbarWidth() + 'px';
     }
 
+    this._handleEscapeKey = (e)=> {
+        if (e.key === 'Escape' && this === Modal.elements[Modal.elements.length - 1]) {
+            this.close();
+        }
+    }
+
     this._onTransitionEnd = (callback) => {
         this._backdrop.ontransitionend = (e) => {
             if (e.propertyName !== 'transform') return;
@@ -153,7 +159,12 @@ function Modal(options = {}) {
     }
 
     this.close = (destroy = destroyOnClose) => {
+        Modal.elements.pop();
         this._backdrop.classList.remove('show');
+
+        if (this._allowEscapeClose) {
+            document.removeEventListener('keydown', this._handleEscapeKey);
+        }
         
         this._onTransitionEnd(() => {
             if (typeof onClose === 'function') onClose();
@@ -164,8 +175,10 @@ function Modal(options = {}) {
             }
     
             // Enable scrolling
-            document.body.classList.remove('no-scroll');
-            document.body.style.paddingRight = '';
+            if (!Modal.elements.length) {
+                document.body.classList.remove('no-scroll');
+                document.body.style.paddingRight = '';
+            }
         });
     }
 }
@@ -173,16 +186,25 @@ function Modal(options = {}) {
 
 const modal1 = new Modal({
         templateId: "#modal-1",
-        destroyOnClose: false,
+        footer: true,
     });
+
+modal1.setFooterContent('<h3>Related Content:</h3>');
+modal1.addFooterButton('Coral Reef', 'modal-btn reef-btn',(e) => {
+    modal2.open();
+})
+modal1.addFooterButton('Kelp Forest', 'modal-btn kelp-btn',(e) => {
+    modal3.open();
+})
+
 $('#open-modal-1').onclick = () => {
     modal1.open()
 }
 
 const modal2 = new Modal({
         templateId: "#modal-2",
-        destroyOnClose: false,
         cssClass: ['class1', 'class2', 'classN'],
+        footer: true,
         onOpen: () => {
             console.log('Modal opened');
         },
@@ -190,60 +212,104 @@ const modal2 = new Modal({
             console.log('Modal closed');
         }
     });
+modal2.setFooterContent('<h3>Related Content:</h3>');
+modal2.addFooterButton('Sandy Beach', 'modal-btn beach-btn',(e) => {
+    modal1.open();
+});
+modal2.addFooterButton('Kelp Forest', 'modal-btn kelp-btn',(e) => {
+    modal3.open();
+});
 $('#open-modal-2').onclick = () => {
     modal2.open()
 }
 
 const modal3 = new Modal({
         templateId: "#modal-3",
-        destroyOnClose: false,
+        footer: true,
     });
+modal3.setFooterContent('<h3>Related Content:</h3>');
+modal3.addFooterButton('Coral Reef', 'modal-btn reef-btn',(e) => {
+    modal2.open();
+});
+modal3.addFooterButton('Seagrass Meadow', 'modal-btn seagrass-btn',(e) => {
+    modal4.open();
+});
 $('#open-modal-3').onclick = () => {
     modal3.open()
 }
 
 const modal4 = new Modal({
         templateId: "#modal-4",
-        destroyOnClose: false,
+        footer: true,
     });
+modal4.setFooterContent('<h3>Related Content:</h3>');
+modal4.addFooterButton('Kelp Forest', 'modal-btn kelp-btn',(e) => {
+    modal3.open();
+});
+modal4.addFooterButton('Epipelagic', 'modal-btn epi-btn',(e) => {
+    modal5.open();
+});
 $('#open-modal-4').onclick = () => {
     modal4.open()
 }
 
 const modal5 = new Modal({
         templateId: "#modal-5",
-        destroyOnClose: false,
+        footer: true,
     });
+modal5.setFooterContent('<h3>Related Content:</h3>');
+modal5.addFooterButton('Seagrass Meadow', 'modal-btn seagrass-btn',(e) => {
+    modal4.open();
+});
+modal5.addFooterButton('Mesopelagic', 'modal-btn meso-btn',(e) => {
+    modal6.open();
+});
 $('#open-modal-5').onclick = () => {
     modal5.open()
 }
 
 const modal6 = new Modal({
         templateId: "#modal-6",
-        destroyOnClose: false,
+        footer: true,
     });
+modal6.setFooterContent('<h3>Related Content:</h3>');
+modal6.addFooterButton('Epipelagic', 'modal-btn epi-btn',(e) => {
+    modal5.open();
+});
+modal6.addFooterButton('Bathypelagic', 'modal-btn bathy-btn',(e) => {
+    modal7.open();
+});
 $('#open-modal-6').onclick = () => {
     modal6.open()
 }
 
 const modal7 = new Modal({
         templateId: "#modal-7",
-        destroyOnClose: false,
+        footer: true,
     });
+modal7.setFooterContent('<h3>Related Content:</h3>');
+modal7.addFooterButton('Mesopelagic', 'modal-btn meso-btn',(e) => {
+    modal6.open();
+});
+modal7.addFooterButton('Abyssopelagic', 'modal-btn abysso-btn',(e) => {
+    modal8.open();
+});
 $('#open-modal-7').onclick = () => {
     modal7.open()
 }
 
 const modal8 = new Modal({
         templateId: "#modal-8",
-        destroyOnClose: false,
         footer: true,
-        closeMethods: []
     });
 
-modal8.addFooterButton('Close', 'modal-btn', (e) => {
-    modal8.close();
-})
+modal8.setFooterContent('<h3>Related Content:</h3>');
+modal8.addFooterButton('Mesopelagic', 'modal-btn meso-btn',(e) => {
+    modal6.open();
+});
+modal8.addFooterButton('Bathypelagic', 'modal-btn bathy-btn',(e) => {
+    modal7.open();
+});
 
 $('#open-modal-8').onclick = () => {
     modal8.open()
